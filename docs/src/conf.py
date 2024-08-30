d69ce397bc49509d13990d0e8a6f5122ff59674a
SKAJohanVenter <aventer@ska.ac.za>
2024-08-30 18:58:47 +0200

    KAR-1011 Updated docs version

--- docs/src/conf.py
+++ docs/src/conf.py
@@ -58,7 +58,7 @@ copyright = "2023, KAROO Team"
 author = "KAROO Team"
 
 # The full version, including alpha/beta/rc tags
-release = "2.0.0"
+release = "0.0.1"
 
 
 # -- General configuration ---------------------------------------------------
